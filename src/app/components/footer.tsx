c035afb829111e7b39b7e29ea61962eb50a62295
macklark <ganesh.kolavennu@proton.me>
2025-05-24 14:55:25 +0530

    added footer section

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return(
+        <p className="tracking-light text-primary-foreground">Built by <a href='https://github.com/macklark' target="_blank" className="hover:underline hover:text-blue-700">@macklark</a> with Next.js and ShadCN UI and deployed on Vercel</p>
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,25 @@ import Connect from './components/connect';
 import Skills from './components/skills';
 import Projects from './components/projects';
 import Work from './components/work';
+import Footer from './components/footer';
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-1 lg:grid-cols-5 gap-8 py-8">
-      <div className="col-span-2 space-y-4 lg:sticky lg:top-8 self-start">
-        <Intro />
-        <Connect />
-      </div>
-      <div className="col-start-3 col-span-5 space-y-8">
-        <Skills />
-        <Projects />
-        <Work />
-      </div>
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <main className="grid grid-cols-1 lg:grid-cols-5 gap-8 py-8 flex-grow">
+        <div className="col-span-2 space-y-4 lg:sticky lg:top-8 self-start">
+          <Intro />
+          <Connect />
+        </div>
+        <div className="col-start-3 col-span-5 space-y-8">
+          <Skills />
+          <Projects />
+          <Work />
+        </div>
+      </main>
+      <footer className="flex justify-center items-center py-4">
+        <Footer />
+      </footer>
+    </div>
   );
 }

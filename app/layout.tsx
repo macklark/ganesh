b6996daa4ec6c38f58dc4a9ae290e2eff1f5f5de
macklark <ganesh.kolavennu@proton.me>
2026-07-13 10:31:37 +0530

    update email id

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
               <rect width="20" height="16" x="2" y="4" rx="2" />
               <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
             </svg>
-            <span className="hidden sm:inline">ganesh.kolavennu@proton.me</span>
+            <span className="hidden sm:inline">hi@ganeshkolavennu.me</span>
           </a>
           <a
             href="https://www.linkedin.com/in/ganesh-kolavennu/"
